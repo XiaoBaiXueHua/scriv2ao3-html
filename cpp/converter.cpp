e47cbc232e55b3cdad33f4a0391a8ea939ecea28
XiaoBaiXueHua <flaminngpancakes@gmail.com>
2024-06-23 13:56:35 -0700

    removed instructions since that was pointless

--- cpp/converter.cpp
+++ cpp/converter.cpp
@@ -26,20 +26,9 @@ using namespace std;
 int main()
 {
 	char ans{'y'}, reorder{'n'}, instr{'n'};
-	bool showIns{true}, gdoc{false};
+	bool gdoc{false};
 	int opt{0};
 	string dir{""};
-	if (!showIns)
-	{
-		showIns = true;
-		std::cout << "Enter Y/y to view instructions/documentation(?), or else hit any key to continue. ";
-		std::cin >> instr;
-		if (toupper(instr) == 'Y')
-		{
-			std::cout << "I'll figure out how to use the .exe file on its own after compiling later. But for now, you need to have at least one folder called \"html\" in the same folder that has this .cpp file.\nYou can have other folders with other html files that can be cleaned and run after you sanitize at least one (1) html file from the \"html\" folder. This is because that's how I hard-coded it :YunJinWink:\nAnyway, you also need to have a folder called \"output\" for your outputs bc I haven't bothered to integrate code that will make a directory if one doesn't yet exist yet because I'm lazy and this project is For Me, Personally, first and foremost.\n\n";
-			std::cout << "Anyway. If you're reading this, you can probably read .cpp files yourself, but I guess to be clear, the `executor()` function is the one that, like, does all the steps of the cleaning. It's at the very bottom of \"styler.cpp\" because even though I am not very good at keeping my code organized as it grows (the README in the \"converter\" folder is arguably the pseudocode. I made no UML bc I've been avoiding that in class like a coward.), I figured that at the very least, I should keep that there at the bottom.\nFunctions are at least groups with their overloads, though they're not organized by the sorts of things they do or even in the same order in the \".cpp\" file as the \".h\" file.\n`findStyle()` and `sanitize()` are, at the very least, kept together. Near the top, far far away from the `executor()` function that uses them.\nI forgot where I was going with this and why I was doing it. But hey! At least we have a logger function uwu.\n\n";
-		}
-	}
 	sClean scriv;
 	project::title("HTML Sanitizer");
 
